52cc32c31f62bdd8ca5fc934b59f568520942605
Julia Mayer <julia.tralalala@gmail.com>
2022-08-12 13:53:32 +0100

    Daycare w/o contacts ratios

--- My model/Seasonal FOI ratios.R
+++ My model/Seasonal FOI ratios.R
@@ -6,7 +6,7 @@
 ################################################################
 setwd("LSHTM/Project/RSV_infection_NL/CSV files")
 getwd()
-trajsim2 <- read.csv("Daycare with addition/add_DEzs_trajsim_ll.csv")
+trajsim2 <- read.csv("Daycare no contacts/add_DEzs_trajsim_ll_noc.csv")
 spring <- trajsim2$P
 winter <- trajsim2$W
 autumn <- trajsim2$A
@@ -30,9 +30,9 @@ ratio_spring_autumn <- plyr::ddply(.data=trajsim2, .variables="time", function(x
 trajsim2$ratioam <- autumn/summer
 ratio_autumn_summer <- plyr::ddply(.data=trajsim2, .variables="time", function(x) quantile(x[,"ratioam"], prob = c(0.025, 0.5, 0.975), na.rm=T)) 
 
-write.csv(ratio_winter_autumn, "add_daycare_ratio_winter_autumn_ll.csv")
-write.csv(ratio_winter_spring, "add__daycare_ratio_winter_spring_ll.csv")
-write.csv(ratio_winter_summer, "add_daycare_ratio_winter_summer_ll.csv")
+write.csv(ratio_winter_autumn, "add_daycare_ratio_winter_autumn_noc.csv")
+write.csv(ratio_winter_spring, "add__daycare_ratio_winter_spring_noc.csv")
+write.csv(ratio_winter_summer, "add_daycare_ratio_winter_summer_noc.csv")
 write.csv(ratio_autumn_summer, "add_ratio_autumn_summer_ll.csv")
 write.csv(ratio_spring_autumn, "new_ratio_autumn_spring_ll.csv")
 write.csv(ratio_spring_summer, "new_ratio_spring_summer_ll.csv")
